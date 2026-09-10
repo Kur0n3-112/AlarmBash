771626249b1737d3a4e8c6c69c6e65dafcbd15b9
Kur0n3-112 <juanriegovila12@gmail.com>
2026-09-10 16:54:22 +0200

    updt(10-Septiembre-2026): Mejorado el codigo con Opencode para probar su funcionamiento

--- timer.sh
+++ timer.sh
@@ -1,110 +1,133 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#Colours
-greenColour="\e[0;32m\033[1m"
-endColour="\033[0m\e[0m"
-redColour="\e[0;31m\033[1m"
-blueColour="\e[0;34m\033[1m"
-yellowColour="\e[0;33m\033[1m"
-purpleColour="\e[0;35m\033[1m"
-turquoiseColour="\e[0;36m\033[1m"
-grayColour="\e[0;37m\033[1m"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-function helpPanel() {
-  echo -e "Uso del script: timer -t 'Nº MINUTOS + FORMATO(s,m,h,d)' "
-  echo -e "\t\ttimer -c 'HORA'"
-  echo -e "\nEjemplo uso script: timer -t 3m"
-  echo -e "\t\t    timer -c 14:00"
+# Colores
+green="\e[1;32m"
+red="\e[1;31m"
+blue="\e[1;34m"
+yellow="\e[1;33m"
+purple="\e[1;35m"
+turquoise="\e[1;36m"
+gray="\e[1;37m"
+reset="\e[0m"
+
+# Recursos
+ALARM_SOUND="$SCRIPT_DIR/alarmSound.mp3"
+BELL_ICON="$HOME/.config/swaync/images/bell.png"
+
+helpPanel() {
+  cat <<EOF
+Uso del script:
+  timer -t 'Nº MINUTOS + FORMATO(s,m,h,d)'   (p. ej. timer -t 3m)
+  timer -c 'HORA'                            (p. ej. timer -c 14:00)
+EOF
 }
 
-function ctrl_c() {
-  echo -e "\n${redColour}[!]Saliendo...\n${endColour}"
-  tput cnorm && exit 1
-  exit 1
+ctrl_c() {
+  echo -e "\n${red}[!] Saliendo...\n${reset}"
+  tput cnorm
+  exit 0
 }
 
-#Ctrl+C
+# Cuenta regresiva: countDown <horas> <minutos> <segundos>
+countDown() {
+  local hours="${1:-0}" minutes="${2:-0}" seconds="${3:-0}"
+  local total=$(( hours * 3600 + minutes * 60 + seconds ))
+
+  while (( total > 0 )); do
+    printf "\r%02d:%02d:%02d" $(( total / 3600 )) $(( (total / 60) % 60 )) $(( total % 60 ))
+    (( total-- ))
+    sleep 1
+  done
+  printf "\n"
+}
+
+ringAlarm() {
+  local elapsed="$1"
+
+  notify-send -u low -i "$BELL_ICON" "Time out! (TimeSpend: $elapsed)"
+  echo -e "\n${yellow}[ALARM] Sonando${reset}"
+  mpv "$ALARM_SOUND" --loop=yes
+}
+
+# Timer por duración: timer -t 3m
+alarm() {
+  local time_to_count="$1"
+  local number unit
+
+  if [[ ! "$time_to_count" =~ ^([0-9]+)([smhd])$ ]]; then
+    echo -e "${red}[!] Formato inválido: '$time_to_count'${reset}" >&2
+    helpPanel
+    exit 1
+  fi
+
+  number="${BASH_REMATCH[1]}"
+  unit="${BASH_REMATCH[2]}"
+
+  echo -e "\nSe va a esperar $time_to_count"
+
+  case "$unit" in
+    s) countDown 0 0 "$number" ;;
+    m) countDown 0 "$number" 0 ;;
+    h) countDown "$number" 0 0 ;;
+    d) countDown $(( number * 24 )) 0 0 ;;
+  esac
+
+  ringAlarm "$time_to_count"
+}
+
+# Timer por hora del día: timer -c 14:00
+hourTimer() {
+  local time_desire="$1"
+  local desired_hour desired_minute now_total target_total minutes_to_wait
+
+  if [[ ! "$time_desire" =~ ^([0-9]{1,2}):([0-9]{2})$ ]]; then
+    echo -e "${red}[!] Hora inválida: '$time_desire'${reset}" >&2
+    helpPanel
+    exit 1
+  fi
+
+  desired_hour="${BASH_REMATCH[1]}"
+  desired_minute="${BASH_REMATCH[2]}"
+
+  echo -e "\nSe va a esperar hasta $time_desire"
+
+  now_total=$(( 10#$(date +%H) * 60 + 10#$(date +%M) ))
+  target_total=$(( 10#$desired_hour * 60 + 10#$desired_minute ))
+
+  # Si la hora ya pasó, esperar hasta el día siguiente
+  if (( target_total <= now_total )); then
+    target_total=$(( target_total + 1440 ))
+  fi
+
+  minutes_to_wait=$(( target_total - now_total ))
+
+  echo "Tiempo: ${minutes_to_wait}m"
+  countDown 0 "$minutes_to_wait" 0
+  ringAlarm "${minutes_to_wait}m"
+}
+
+main() {
+  local mode=""
+  local value=""
+
+  while getopts "t:c:h" opt; do
+    case "$opt" in
+      t) mode="t"; value="$OPTARG" ;;
+      c) mode="c"; value="$OPTARG" ;;
+      h) helpPanel; exit 0 ;;
+      *) helpPanel; exit 1 ;;
+    esac
+  done
+
+  case "$mode" in
+    t) alarm "$value" ;;
+    c) hourTimer "$value" ;;
+    *) helpPanel ;;
+  esac
+}
+
 trap ctrl_c INT
 
-function alarm() {
-  timeToCount="$1"
-
-  echo -e "\nSe va a esperar $timeToCount"
-
-  sleep $timeToCount && notify-send -u low -i "$HOME/.config/swaync/images/bell.png" "Time out!(TimeSpend: $timeToCount)"
-  echo -e "\n${yellowColour}[ALARM]Sonando${endColour}"
-  cvlc --repeat "$HOME/Documentos/Scripts/alarm/alarmSound.mp3"
-
-
-}
-
-function hourTimer() {
-  timeDesire="$1"
-
-  echo -e "\nSe va ha esperar hasta $timeDesire"
-
-  currentHour="$(date +"%H")"
-  currentMinute="$(date +"%M")"
-
-  #Solo el valor de la hora cuando introducida por consola
-  hoursInDesire="$(echo "$timeDesire" | awk '{print $1}' | cut -c 1-2)"
-  #El valor de los minutos introducidos por consola
-  minutesInDesire="$(echo "$timeDesire" | awk '{print $1}' | cut -c 4-5)"
-
-  if [ $hoursInDesire -eq $currentHour ]; then
-    if [ $currentMinute -gt $minutesInDesire ]; then
-      esperaMinutos="$(echo "$currentMinute - $minutesInDesire" | bc)"
-    fi
-    if [ $currentMinute -lt $minutesInDesire ]; then
-      esperaMinutos="$(echo "$minutesInDesire - $currentMinute" | bc)"
-    fi
-  fi
-
-  if [ $hoursInDesire -gt $currentHour ]; then
-
-    mirarSiHoraMayor="$(echo "$hoursInDesire - $currentHour" | bc)"
-
-    if [ $mirarSiHoraMayor -gt 1 ]; then
-      horasEsperar="$(echo "60 * $mirarSiHoraMayor" | bc)"
-      currentMinuteMinusHour="$(echo "60 - $currentMinute " | bc)"
-      esperaMinutos="$(echo "$currentMinuteMinusHour + $minutesInDesire + $horasEsperar" | bc)"
-    else
-      currentMinuteMinusHour="$(echo "60 - $currentMinute " | bc)"
-      esperaMinutos="$(echo "$currentMinuteMinusHour + $minutesInDesire" | bc)"
-    fi
-  fi
-
-  fixesperaMinutos="$(echo "$esperaMinutos m" | tr -d ' ')"
-
-  echo "Tiempo: $fixesperaMinutos"
-
-  sleep $fixesperaMinutos && notify-send -u low -i "$HOME/.config/swaync/images/bell.png" "Time out!(TimeSpend: $fixesperaMinutos)"
-  echo -e "\n${yellowColour}[ALARM]Sonando${endColour}"
-  cvlc --repeat "$HOME/Documentos/Scripts/\[01\]\ Kur/alarm/alarmSound.mp3" 2>/dev/null
-}
-
-# Indicators
-declare -i parameter_counter=0
-
-while getopts "t:c:h" arg; do
-  case $arg in
-  t)
-    timeToCount="$OPTARG"
-    let parameter_counter+=1
-    ;;
-  c)
-    timeDesire="$OPTARG"
-    let parameter_counter+=2
-    ;;
-
-  h) ;;
-  esac
-done
-
-if [ $parameter_counter -eq 1 ]; then
-  alarm $timeToCount
-elif [ $parameter_counter -eq 2 ]; then
-  hourTimer $timeDesire
-else
-  helpPanel
-fi
+main "$@"
